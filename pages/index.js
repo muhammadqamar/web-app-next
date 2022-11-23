5c6f6372357fbb2ec330fe6896c34257c6d1eccc
Muhammad Qamar <muhammadqamar111@gmail.com>
2022-11-23 16:47:24 +0500

    FEAT:auth

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { auth } from '../service/firebase/firebase';
+import { signInWithPopup, GoogleAuthProvider,FacebookAuthProvider } from 'firebase/auth';
+import { async } from '@firebase/util';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const [user, setUser] = useAuthState(auth);
+ 
+  const handlerWithGoogleLogin = async () => {
+    const googleAuth = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, googleAuth);
+  };
+
+  const handlerWithFacebookLogin= async()=>{
+    const facebookAuth = new  FacebookAuthProvider();
+     const result = await signInWithPopup(auth, facebookAuth);
+  }
+  const logout=()=>{
+    auth.signOut()
+  }
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,59 +37,21 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome <span>{user?.displayName}</span>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.btn_div}>
+          {user ? (
+            <>
+              <button onClick={logout}>logout</button>
+            </>
+          ) : (
+            <>
+              <button onClick={handlerWithGoogleLogin}>login With Google</button>
+              <button onClick={handlerWithFacebookLogin}>login With FaceBook</button>
+            </>
+          )}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
---
+++ service/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDui7adeIonqMZjcS1tli3WlmiOUQQv0A",
+  authDomain: "web-app-next.firebaseapp.com",
+  projectId: "web-app-next",
+  storageBucket: "web-app-next.appspot.com",
+  messagingSenderId: "745586650657",
+  appId: "1:745586650657:web:c80fc7fb126ecea6e17d60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth=getAuth()
